8403a52d965ebef79207d940af651d9951a88210
jackzoom <676221604@qq.com>
2017-08-16 00:04:46 +0800

    succeed_0815

--- js/showImg.js
+++ js/showImg.js
@@ -12,11 +12,15 @@ var clickCount=1;
 getBtn.addEventListener("click",function(){
     if(clickCount==1){
         getImg.setAttribute("src","img/ver.png");
+        getImg.setAttribute("width","320");
+        getImg.setAttribute("height","432");
         clickCount=2;
     }
     else
     {
         getImg.setAttribute("src","img/hor.png");
+        getImg.setAttribute("width","360");
+        getImg.setAttribute("height","225");
         clickCount=1;
     }
 },false)
@@ -29,8 +33,8 @@ function picBig(){
 
     var getImg_W=getImg.width;//图片原始宽度
     var getImg_H=getImg.height;//图片原始宽高度
-    var getDiv_W=getoutLine.offsetWidth-1;//div宽度
-    var getDiv_H=getoutLine.offsetHeight-1;//div高度
+    var getDiv_W=getoutLine.offsetWidth;//div宽度
+    var getDiv_H=getoutLine.offsetHeight;//div高度
     var ratio_W =getDiv_W/getImg_W;//宽度比例
     var ratio_H =getDiv_H/getImg_H;//高度比例
     console.log("原始图片宽高："+getImg_W+"*"+getImg_H);
@@ -39,24 +43,28 @@ function picBig(){
     console.log("高度比例："+ratio_H);
 
 
-    if(getImg_W>getImg_H){
-        getImg_W=getImg_W*ratio_W+"px";
-        getImg_H=getImg_H*ratio_W+"px";
+    if(getImg_W>=getImg_H){
+        getImg_W=parseInt(getImg_W*ratio_W)+"px";
+        getImg_H=parseInt(getImg_H*ratio_W)+"px";
         console.log("宽比高大");
-        console.log(getImg_W);
-        console.log(getImg_H);
+        // console.log("宽："+getImg_W);
+        // console.log("高："+getImg_H);
         getImg.setAttribute("width",getImg_W);
         getImg.setAttribute("height",getImg_H);
+        console.log("修改后的宽:"+getImg.width);
+        console.log("修改后的高:"+getImg.height);
     }
     else
     {
-        getImg_H=getImg_H*ratio_W+"px";
-        getImg_W=getImg_W*ratio_H+"px";
+        getImg_H=parseInt(getImg_H*ratio_H)+"px";
+        getImg_W=parseInt(getImg_W*ratio_H)+"px";
         console.log("高比宽大");
-        console.log(getImg_W);
-        console.log(getImg_H);
-        getImg.setAttribute("height",getImg_H);
+        // console.log("宽："+getImg_W);
+        // console.log("高："+getImg_H);
         getImg.setAttribute("width",getImg_W);
+        getImg.setAttribute("height",getImg_H);
+        console.log("修改后的宽:"+getImg.width);
+        console.log("修改后的高:"+getImg.height);
     }
 }
 
@@ -82,7 +90,7 @@ function getStyle(obj,attr) {
     else
     {
         //非火狐浏览器
-        return parseInt(getComputedStyle(obj,false)[attr]);
+        return parseInt(getComputedStyle(obj)[attr]);
     }
     
 }
